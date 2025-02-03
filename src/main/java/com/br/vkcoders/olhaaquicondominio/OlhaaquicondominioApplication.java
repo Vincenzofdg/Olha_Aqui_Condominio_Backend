3f23c4909156953e45ad132c0a1b57fd43faaefa
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-02-03 02:57:05 -0300

    feat: starting spring boot application

---
+++ src/main/java/com/br/vkcoders/olhaaquicondominio/OlhaaquicondominioApplication.java
@@ -0,0 +1,13 @@
+package com.br.vkcoders.olhaaquicondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlhaaquicondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlhaaquicondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/vkcoders/olhaaquicondominio/OlhaaquicondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vkcoders.olhaaquicondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlhaaquicondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
